a8ed3e2b13b186b489572e05704599a2e7fd89c8
bodhibrdr <julie.gumerman@gmail.com>
2020-07-18 13:06:09 -0600

    body parts in a hashtable!

--- src/dem_bones_gonna_walk_around/bones.scala
+++ src/dem_bones_gonna_walk_around/bones.scala
@@ -2,22 +2,34 @@ package dem_bones_gonna_walk_around
 
 object bones extends App{
 
+  //body parts
+
   var body = Array("toe", "foot", "ankle", "leg", "knee", "thigh", "hip", "back", "neck", "head", "finger", "hand", "arm", "shoulder")
 
+  //hash table! Whoo!!!
+
+  val collectionOfBodyParts = scala.collection.mutable.HashMap.empty[Int,Bone]
+
+  //get body parts from array into classes; get classes into a hashtable
+
   for (i <- 0 until body.length -1) {
-//    var newBone = new Bone (body(i))
-//    println(newBone.bone)
     if (i == 0) {
       var newBone = new Bone ("none", body(i))
       println(newBone.prev, newBone.bone)
+      collectionOfBodyParts += ((i) -> newBone)
     }
     else {
-      var newBone = new Bone(body(i-1), body(i))
+      var newBone = new Bone(body(i), body(i))
       println(newBone.prev, newBone.bone)
+      collectionOfBodyParts += ((i) -> newBone)
     }
   }
 
+  println(collectionOfBodyParts)
+
 }
+
+//"body" class
 
 class Bone (val prev: String, val bone: String) {
 
