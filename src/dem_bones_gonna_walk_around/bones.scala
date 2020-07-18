3e66ac4cf95db8722abe27a8b81cb06158b42880
bodhibrdr <julie.gumerman@gmail.com>
2020-07-18 12:52:16 -0600

    creating instances of classes

--- src/dem_bones_gonna_walk_around/bones.scala
+++ src/dem_bones_gonna_walk_around/bones.scala
@@ -4,11 +4,22 @@ object bones extends App{
 
   var body = Array("toe", "foot", "ankle", "leg", "knee", "thigh", "hip", "back", "neck", "head", "finger", "hand", "arm", "shoulder")
 
-  body.foreach(i in body => println(new Bone(i)))
+  for (i <- 0 until body.length -1) {
+//    var newBone = new Bone (body(i))
+//    println(newBone.bone)
+    if (i == 0) {
+      var newBone = new Bone ("none", body(i))
+      println(newBone.prev, newBone.bone)
+    }
+    else {
+      var newBone = new Bone(body(i-1), body(i))
+      println(newBone.prev, newBone.bone)
+    }
+  }
 
 }
 
-class Bone (bone: String) {
+class Bone (val prev: String, val bone: String) {
 
 
 }
